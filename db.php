7b0096067d234722e4bbd0ef441af0f9944a02ac
Facumiranda <127260350+Facumiranda@users.noreply.github.com>
2025-06-17 18:05:47 -0300

    Add files via upload
    Se agrego el login con las validaciones y redirecciones apropiadas

---
+++ db.php
@@ -0,0 +1,17 @@
+<?php
+// Conf de la configuracion de la base de datos
+$_nombreServer = "localhost"; // Servidor de la base de datos
+$_nombreUsuario = "root"; // Usuario de la base
+$_contraseña = ""; // Constraseña de la base
+$_dbNombre = "cv_labo_p2"; // Nombre de la base
+
+// Crear la conexion
+$_conexionDB = new mysqli($_nombreServer, $_nombreUsuario, $_contraseña, $_dbNombre);
+
+// Verificar la conexion
+if ($_conexionDB->connect_error) {
+    die("Error de conexion: " . $_conexionDB->connect_error);
+}
+
+// Establecemos charset (caracteres españoles)
+$_conexionDB->set_charset("utf8");
---
+++ header.php
@@ -0,0 +1,30 @@
+<header>
+    <!-- Barra de navegación -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary d-print-none">
+        <div class="container">
+            <a class="navbar-brand">CVC - Panel de Administrador</a>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#experiencia">Experiencia</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#habilidades">Habilidades</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#educacion">Educación</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="vistaPrevia.php">Vista previa</a>
+                        <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#logoutModal">
+                                <i class="fas fa-user-lock"></i> Cerrar sesión
+                            </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+</header>
